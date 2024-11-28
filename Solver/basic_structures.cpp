5736ea1b15228624af259ca5802f71f5792f1788
Timmifixedit <tim@luchterhand.org>
2024-11-28 11:13:04 +0100

    added empty methods to basic structures

--- Solver/basic_structures.cpp
+++ Solver/basic_structures.cpp
@@ -5,7 +5,42 @@
 */
 
 #include "basic_structures.hpp"
+#include "util/random.hpp"
 
 namespace sat {
     // @TODO implementation here
+
+
+    Variable::Variable(unsigned val) {
+    }
+
+    unsigned Variable::get() const {
+    }
+
+    bool Variable::operator==(const Variable &other) const {
+    }
+
+    Literal::Literal(unsigned val) {
+    }
+
+    unsigned Literal::get() const {
+    }
+
+    Literal Literal::negate() const {
+    }
+
+    short Literal::sign() const {
+    }
+
+    bool Literal::operator==(const Literal &other) const {
+    }
+
+    Literal pos(Variable x) {
+    }
+
+    Literal neg(Variable x) {
+    }
+
+    Variable var(Literal l) {
+    }
 }
