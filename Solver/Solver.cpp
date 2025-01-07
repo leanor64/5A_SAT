3f54a524ab3e2d0dc8113d9c8654ead6616cdfd3
norgeux <norgeux@insa-toulouse.fr>
2025-01-07 15:22:52 +0100

    manque plus que rebase et unitPropagation

--- Solver/Solver.cpp
+++ Solver/Solver.cpp
@@ -17,7 +17,7 @@ namespace sat {
     bool Solver::addClause(Clause clause) {
         bool res = false;
         if (clause.size() == 1) {
-            assign(*clause.begin())
+            assign(*clause.begin());
         } else if (!clause.isEmpty()) {
             auto ptr = std::make_shared<Clause>(clause); 
             clauses.emplace_back(ptr);
@@ -31,14 +31,26 @@ namespace sat {
     }
 
     auto Solver::rebase() const -> std::vector<Clause> {
-        std::vector<Clause> newClauses;
-        for (TruthValue val : model) {
-            if () {
-                
+        std::vector<Clause> newClauses ;
+        for (ConstClausePointer clause : clauses) {
+            bool satisfiedClause = false;
+            std::vector<Literal> finalClause;
+            for (Literal lit : *clause ) { //watchers ?
+                if (satisfied(lit)) {
+                    satisfiedClause = true;
+                    break;
+                } else if (!falsified(lit)){
+                    finalClause.emplace_back(lit);
+                }
+            }
+            if (!satisfiedClause && !(finalClause.empty())) {
+                newClauses.emplace_back(finalClause);
             }
         }
-
-
+        
+        for (Literal unitL : unitLiterals) {
+            newClauses.emplace_back(Clause({unitL}));
+        }
         return newClauses;
     }
 
@@ -47,15 +59,33 @@ namespace sat {
     }
 
     bool Solver::satisfied(Literal l) const {
-        return l.sign() == 1;
+        if (l.sign() == 1) {
+            return val(var(l)) == sat::TruthValue::True;
+        } else {
+            return val(var(l)) == sat::TruthValue::False;
+        }
     }
 
     bool Solver::falsified(Literal l) const {
-        throw NOT_IMPLEMENTED;
+        if (l.sign() == -1) {
+            return val(var(l)) == sat::TruthValue::True;
+        } else {
+            return val(var(l)) == sat::TruthValue::False;
+        }
     }
 
     bool Solver::assign(Literal l) {
-        throw NOT_IMPLEMENTED;
+        if (l.sign() == -1 && val(var(l))!=sat::TruthValue::Undefined){ 
+            return false;
+        } else {
+            if (l.sign() == 1) {
+                model[var(l).get()]=sat::TruthValue::True;
+            } else {
+                model[var(l).get()]=sat::TruthValue::False;
+            }
+            unitLiterals.push_back(l);
+            return true;
+        }
     }
 
     bool Solver::unitPropagate() {
